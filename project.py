45caca2fd03e5da06e1c716acc06fc5d5c80aafa
alicea13 <alisa-ays@mail.ru>
2019-12-26 15:11:01 +0300

    Классы Character, AnimatedSprite

--- project.py
+++ project.py
@@ -23,7 +23,7 @@ class StartWindow:
         ask_input = pygame.font.SysFont('arial', 25)
         self.text2 = ask_input.render("Введите логин:", 1, pygame.Color("lightblue"))
 
-        login = InputText(235, 230, 140, 32)
+        login = InputText(width // 2 - 70, height // 5 * 3 - 32, 140, 32)
 
         run_st = True
 
@@ -77,13 +77,11 @@ class InputText:
                                          (login_t,)).fetchall()
         print(self.id_login)
         if self.id_login != []:
-            self.l, = self.id_login[0] ###
+            self.l, = self.id_login[0]
 
             self.have_login = HaveLogin(login_t)
         else:
             self.no_login = NoLogin(login_t)
-            #self.no_login.show()
-            #self.close()
 
     def update(self):
         width = max(200, self.text_surf.get_width() + 8)
@@ -107,7 +105,8 @@ class HaveLogin:
         self.text1 = titl.render("Добро ожаловать,", 1, pygame.Color("blue"))
 
         log = pygame.font.SysFont('colibri', 50)
-        self.text2 = log.render(login, 1, pygame.Color('lightblue'))
+        self.text2 = log.render(login, 1, pygame.Color('lightblue'))   # login.rjust((width - 435) % 8, " ")
+
 
         act_open = pygame.font.SysFont("arial", 25)
         self.text3 = act_open.render(f"Начать игру", 1, pygame.Color("lightblue"))
@@ -127,9 +126,9 @@ class HaveLogin:
                     run = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     '''if 160 <= event.pos[0] <= 450 and 160 <= event.pos[1] <= 213:
-                        # открытие окна с полем
+                        # открытие окна с полем'''
                     if 160 <= event.pos[0] <= 450 and 160 <= event.pos[1] <= 283:
-                        # открытие окна с персоажами'''
+                        char = Character()
                     if 160 <= event.pos[0] <= 450 and 160 <= event.pos[1] <= 353:
                         run = False
                         # выход к стартовому окну
@@ -137,6 +136,7 @@ class HaveLogin:
             for i in range(3):
                 pygame.draw.rect(screen, pygame.Color("blue"),
                                  (160, 160 + i * 70, 290, 53), 3)
+
             screen.blit(self.text1, (100, 60))
             screen.blit(self.text2, (435, 65))
             screen.blit(self.text3, (230, 170))
@@ -144,10 +144,77 @@ class HaveLogin:
             screen.blit(self.text5, (175, 310))
             pygame.display.flip()
 
+
+class Character:
+    def __init__(self):
+        super().__init__()
+        pygame.init()
+
+        size = width, height = 600, 500
+        screen = pygame.display.set_mode(size)
+
+        self.all_sprites = pygame.sprite.Group()
+
+        snake = AnimatedSprite(self.load_image("snake_sm.png"), 4, 2, 50, 150, self.all_sprites)
+        clock = pygame.time.Clock()
+
+        run_spr = True
+
+        while run_spr:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run_spr = False
+            screen.fill((0, 0, 0))
+            self.all_sprites.draw(screen)  # говорим прорисовывать sprites
+            self.all_sprites.update()
+            pygame.display.flip()
+            clock.tick(15)  # для того чтобы не тормозить программу в целом, можно создать счетчик в классе AnimatedSprite
+
+    def load_image(self, name, color_key=None):
+        fullname = os.path.join("data", name)
+        image = pygame.image.load(fullname).convert()
+        if color_key is not None:
+            if color_key == -1:
+                color_key = image.get_at((0, 0))
+            image.set_colorkey(color_key)
+        else:
+            image = image.convert_alpha()
+        return image
+
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y, group):
+        super().__init__(group)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+
 class NoLogin:
     def __init__(self, login):
         super().__init__()
         pygame.init()
+
+        self.con = sqlite3.connect("one_little_worm.db")
+        self.cur = self.con.cursor()
+
+        '''self.log_app = self.cur.execute("INSERT INTO logins VALUES(login)").fetchall()
+        self.con.commit()'''
 
         size = width, height = 600, 500
         screen = pygame.display.set_mode(size)
@@ -156,7 +223,7 @@ class NoLogin:
         self.text1 = titl.render("Новый игрок,", 1, pygame.Color("blue"))
 
         log = pygame.font.SysFont('colibri', 50)
-        self.text2 = log.render(login, 1, pygame.Color('lightblue'))
+        self.text2 = log.render(login, 1, pygame.Color('lightblue'))   # login.rjust((width - 435) % 8, " ")
 
         act_open = pygame.font.SysFont("arial", 25)
         self.text3 = act_open.render(f"Начать игру", 1, pygame.Color("lightblue"))
@@ -165,8 +232,7 @@ class NoLogin:
         self.text4 = act_chg.render("Выбрать персонажа", 1, pygame.Color("lightblue"))
 
         act_exit = pygame.font.SysFont("arial", 25)
-        self.text5 = act_exit.render("Сменить пользователя", 1,
-                                    pygame.Color("lightblue"))
+        self.text5 = act_exit.render("Сменить пользователя", 1, pygame.Color("lightblue"))
 
         run = True
 
@@ -176,9 +242,9 @@ class NoLogin:
                     run = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     '''if 160 <= event.pos[0] <= 450 and 160 <= event.pos[1] <= 213:
-                        # открытие окна с полем
+                        # открытие окна с полем'''
                     if 160 <= event.pos[0] <= 450 and 160 <= event.pos[1] <= 283:
-                        # открытие окна с персоажами'''
+                        char = Character()
                     if 160 <= event.pos[0] <= 450 and 160 <= event.pos[1] <= 353:
                         run = False
                         # выход к стартовому окну
@@ -193,4 +259,11 @@ class NoLogin:
             screen.blit(self.text5, (175, 310))
             pygame.display.flip()
 
+
+
+
 start = StartWindow()
+
+
+# меню с настройками
+# карта для перехода между режимами игры
